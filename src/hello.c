d18e7b002233390c0cc05dac1e5122e33dc3463a
TheSparkMods <thesparkmods@gmail.com>
2016-07-14 15:03:21 -0500

    Add files via upload

---
+++ src/hello.c
@@ -0,0 +1,17 @@
+/*
+ * hello.c
+ *
+ *  Created on: Jul 14, 2016
+ *      Author: Anthony
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	printf("Hello World");
+
+	getch();
+	return 0;
+}
